b15cb3fc5dec22d6ae5fdc6fd965847a484782eb
Asier <aarpon002@ikasle.ehu.eus>
2020-04-23 20:02:33 +0200

    v1.8

--- LOOKCONCERT/src/bl/ErabiltzaileaEJB.java
+++ LOOKCONCERT/src/bl/ErabiltzaileaEJB.java
@@ -1,6 +1,7 @@
 package bl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -60,4 +61,14 @@ public class ErabiltzaileaEJB {
 		else kodea=1; //Irakasgai hori ez da existitzen DB-an
 		return kodea;
 	}
+    public int kontzertuaSartuDB(Date data, String lekua, float sarreraPrezioa, String sarreraEsteka, int sarreraKop) {
+    	int kodea=0;
+    	erabiltzaileaE.getTaldePartaideakE().setTaldeakE(em.find(TaldeakE.class,erabiltzaileaE.getTaldePartaideakE().getTaldeakE().getIzena()));
+    	if(erabiltzaileaE.getTaldePartaideakE().getTaldeakE()==null) kodea=1;//taldea ez da existitzen
+    	else {
+    	KontzertuakE kontzertuaE = new KontzertuakE(0,data,lekua,sarreraPrezioa,sarreraEsteka,sarreraKop,erabiltzaileaE.getTaldePartaideakE().getTaldeakE());
+    	em.persist(kontzertuaE);
+    	}
+    	return kodea;
+    }
 }
