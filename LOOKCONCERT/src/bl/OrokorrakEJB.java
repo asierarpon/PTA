d4ac3e88248cff43c316174c148b2d7ad22269f7
Asier <aarpon002@ikasle.ehu.eus>
2020-05-01 21:37:09 +0200

    v3.2

--- LOOKCONCERT/src/bl/OrokorrakEJB.java
+++ LOOKCONCERT/src/bl/OrokorrakEJB.java
@@ -162,7 +162,6 @@ public class OrokorrakEJB {
     }
     @SuppressWarnings("unchecked")
 	public List<TaldeakE> modaLortuDB(){
-    	return (List<TaldeakE>)em.createNamedStoredProcedureQuery("findModa").setMaxResults(5).getResultList();
-    	//return (List<TaldeakE>)em.createNamedQuery("GustokoenakE.findModa").setMaxResults(5).getResultList();
+    	return (List<TaldeakE>)em.createNamedStoredProcedureQuery("findModa").getResultList();
     }
 }
